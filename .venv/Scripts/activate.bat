72e6ee107c6480e57c565a873520ce8f9227e195
Utkarsh Solanki <160130824+UtkarshSolanki07@users.noreply.github.com>
2025-10-12 14:00:29 +0530

    ...

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DELL\Desktop\AI ML SEGEMENTATION\.venv
+set VIRTUAL_ENV=c:\Users\DELL\Desktop\AI ML SEGEMENTATION\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
